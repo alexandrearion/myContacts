832d29c740dc0ddc829cab370dede0f69867dce7
alexandrearion <alexandrearion.94@gmail.com>
2025-01-20 21:28:01 -0300

    categories module

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+const CategoriesRespository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRespository.findAll();
+
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error:'Name is required' });
+    }
+
+    const category = await CategoriesRespository.create({ name });
+
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
